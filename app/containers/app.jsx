5e237b95f205089f60305c64760530db298274e9
siemiatj <kuba@saucelabs.com>
2017-03-31 17:09:47 +0200

    - basic React scaffolding

---
+++ app/containers/app.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+	render() {
+		return (
+			<table className="widefat">
+				<thead>
+					<tr>
+						<th>ID</th>
+						<th>Title</th>
+						<th>Link</th>
+						<th>Expires</th>
+						<th colspan="2" className="actions">Actions</th>
+					</tr>
+				</thead>
+			</table>
+		);
+	}
+}
