23b416d03b957b7c3724dc58d5eae056848fc2ab
siemiatj <kuba@saucelabs.com>
2017-04-02 17:04:24 -0700

    - production and dev webpack builds - modify eslintrc

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,45 @@
 const path = require('path');
+const webpack = require('webpack');
 
 const config = {
-  devtool: 'source-map',
-  name: 'browser',
-  entry: {
-    app: './app/index',
-  },
-  output: {
-    path: path.join(__dirname, 'build'),
-    filename: 'bundle.js',
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$|\.jsx$/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015', 'react', 'stage-0']
-        },
-        include: path.join(__dirname, 'app'),
-        exclude: path.join(__dirname, '/node_modules/'),
-      },
-      {
-        test: /\.css$/,
-        loaders: ['style-loader', 'css-loader']
-      },
-    ]
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.css'],
-    modules: [
-      'app', 'node_modules'
-    ],
-  },
+	devtool: 'eval-source-map',
+	name: 'browser',
+	entry: {
+		app: './app/index',
+	},
+	output: {
+		path: path.join( __dirname, 'build' ),
+		filename: 'bundle.js',
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$|\.jsx$/,
+				loader: 'babel-loader',
+				query: {
+					presets: ['es2015', 'react', 'stage-0'],
+					plugins: ['transform-object-rest-spread'],
+				},
+				include: path.join( __dirname, 'app' ),
+				exclude: path.join( __dirname, '/node_modules/' ),
+			},
+			{
+				test: /\.css$/,
+				loaders: ['style-loader', 'css-loader']
+			},
+		]
+	},
+	resolve: {
+		extensions: ['.js', '.jsx', '.css'],
+		modules: [
+			'app', 'node_modules'
+		],
+	},
+	plugins: [
+		new webpack.DefinePlugin( {
+			__DEV__: process.env.NODE_ENV !== 'production',
+		} ),
+	],
 };
 
 module.exports = config;
